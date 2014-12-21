f3527b65f3836d61056bbf266e00df2bf93f25dc
jana <jana16@meet.mit.edu>
2014-12-21 13:32:50 +0200

    meet conf

---
+++ meetconf/MEETconf.py
@@ -0,0 +1,25 @@
+import turtle
+def draw_circle(x,y,s):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.circle(150,steps=6)
+draw_circle(100,100,100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.mainloop() 
+
