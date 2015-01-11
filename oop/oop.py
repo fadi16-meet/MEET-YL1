98a2dcb571b44198415c44b87921fc754f306751
Fadi Arafeh <fadi16@meet.mit.edu>
2015-01-11 19:20:27 +0200

    oop the zoo

--- oop/oop.py
+++ oop/oop.py
@@ -3,18 +3,26 @@ class Animal :
           self.name=name
           self.color=color
           self.age=age
-          self.size=size 
-    
+          self.size=size
+          
     def print_all(self):
           print(self.name)
           print(self.color)
           print(self.age)
           print(self.size)
+         
+    def eat(self,food):
+          print(self.name, "is eating",food)
+    def sleep(self):
+          print(self.name, "is sleeping")
+    def breathe(self):
+          print(self.name, "is breathing")         
+          
+Dog=Animal(name="sasha",color="black",age=4,size="huge")
+Cat=Animal(name="lulu",color="white",age=2,size="tiny")
 
-Dog=Animal(name="max",color="black",age=4,size="huge")
-Cat=Animal(name="min",color="white",age=2,size="tiny")
-
-
+Dog.eat(food="pizza")
+Cat.eat(food="fish")
 Dog.print_all()
 Cat.print_all()
 
